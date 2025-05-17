8f144b5c31b8be6feb5981b20094a9eb22288168
Kevin Uduji <kevinuduji@gmail.com>
2025-05-16 23:23:43 -0500

    fix metadata image and text when sent through different platfoms

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,30 @@ export const metadata = {
   description: "Kevin Uduji's Career Portfolio",
   icons: {
     icon: "/kevin.jpg",
+    apple: "/kevin.jpg", // Apple touch icon
+  },
+  openGraph: {
+    type: "website",
+    url: "https://kevinuduji-github-io.vercel.app/",
+    title: "Kevin's Portfolio",
+    description: "Kevin Uduji's Career Portfolio",
+    images: [
+      {
+        url: "https://og-image.vercel.app/api/screenshot?url=https://kevinuduji-github-io.vercel.app/",
+        width: 1200,
+        height: 630,
+        alt: "Kevin Uduji's Portfolio Live Preview",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@yourtwitter", // Optional: your Twitter handle
+    title: "Kevin's Portfolio",
+    description: "Kevin Uduji's Career Portfolio",
+    images: [
+      "https://og-image.vercel.app/api/screenshot?url=https://kevinuduji-github-io.vercel.app/",
+    ],
   },
 };
 
@@ -25,6 +49,31 @@ export default function RootLayout({ children }) {
     <html lang="en" className="dark scroll-smooth">
       <Head>
         <link rel="icon" href="/kevin.jpg" />
+        <link rel="apple-touch-icon" href="/kevin.jpg" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://kevinuduji-github-io.vercel.app/"
+        />
+        <meta property="og:title" content="Kevin's Portfolio" />
+        <meta
+          property="og:description"
+          content="Kevin Uduji's Career Portfolio"
+        />
+        <meta
+          property="og:image"
+          content="https://og-image.vercel.app/api/screenshot?url=https://kevinuduji-github-io.vercel.app/"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Kevin's Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Kevin Uduji's Career Portfolio"
+        />
+        <meta
+          name="twitter:image"
+          content="https://og-image.vercel.app/api/screenshot?url=https://kevinuduji-github-io.vercel.app/"
+        />
       </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
